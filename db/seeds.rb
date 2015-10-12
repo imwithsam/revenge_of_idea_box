2c943490ad7811304d921bf22d51b6ff486e85b8
Samson Brock <SamsonLB@gmail.com>
2015-10-12 16:51:22 -0600

    Create Idea model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do |i|
+  Idea.create(
+    title: "Title #{i+1}",
+    body:  "One morning, when Gregor Samsa woke from troubled dreams, he" \
+           " found himself transformed in his bed into a horrible vermin."
+  )
+end

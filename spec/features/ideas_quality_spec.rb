222ef34e26770677c159159f3099da54cd1481ac
Samson Brock <SamsonLB@gmail.com>
2015-10-14 16:46:10 -0600

    Add spec for liking an idea twice

--- spec/features/ideas_quality_spec.rb
+++ spec/features/ideas_quality_spec.rb
@@ -1,15 +1,28 @@
 require "rails_helper"
 
 RSpec.feature "Ideas quality", type: :feature do
-  scenario "User likes an existing idea", js: true do
+  before do
     visit root_path
 
     fill_in("title", with: "Title 1")
     fill_in("body", with: "Body 1")
     click_button("Save")
+  end
+
+  scenario "User likes an existing idea", js: true do
     click_link("Like")
 
     expect(page).to_not have_content("swill")
+    expect(page).to_not have_content("genius")
     expect(page).to have_content("plausible")
+  end
+
+  scenario "User likes an existing idea twice", js: true do
+    click_link("Like")
+    click_link("Like")
+
+    expect(page).to_not have_content("swill")
+    expect(page).to_not have_content("plausible")
+    expect(page).to have_content("genius")
   end
 end

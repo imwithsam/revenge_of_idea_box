9dc67f8a13f61a227611ba8f7bf4c8afd2380bdb
Samson Brock <SamsonLB@gmail.com>
2015-10-14 20:29:30 -0600

    Add cancel to update form.

--- spec/features/idea_editing_spec.rb
+++ spec/features/idea_editing_spec.rb
@@ -1,14 +1,16 @@
 require "rails_helper"
 
 RSpec.feature "Idea editing", type: :feature do
-  scenario "User edits an existing idea", js: true do
+  before do
     visit root_path
 
     fill_in("title", with: "Title 1")
     fill_in("body", with: "Body 1")
     click_button("Save")
     click_link("Edit")
+  end
 
+  scenario "User edits an existing idea", js: true do
     fill_in("Title", with: "New Title")
     fill_in("Body", with: "New Body")
     click_button("Update")
@@ -17,5 +19,16 @@ RSpec.feature "Idea editing", type: :feature do
     expect(page).to_not have_content("Body 1")
     expect(page).to have_content("New Title")
     expect(page).to have_content("New Body")
+  end
+
+  scenario "User cancels an edit", js: true do
+    fill_in("Title", with: "New Title")
+    fill_in("Body", with: "New Body")
+    click_link("Cancel")
+
+    expect(page).to_not have_content("New Title")
+    expect(page).to_not have_content("New Body")
+    expect(page).to have_content("Title 1")
+    expect(page).to have_content("Body 1")
   end
 end
